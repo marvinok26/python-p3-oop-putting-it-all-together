15a83be9575bbd12e372cae77b0843aeb04beecd
Marvin <okongomarvin971@gmail.com>
2024-03-07 08:44:33 +0300

    Completed Assignment

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,35 @@
-#!/usr/bin/env python3
-
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        self._title = title
+        self._page_count = page_count
+
+    @property
+    def title(self):
+        return self._title
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if isinstance(value, int):
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+# Example usage
+if __name__ == "__main__":
+
+    book1 = Book("And Then There Were None", 272)
+    assert book1.page_count == 272
+    assert book1.title == "And Then There Were None"
+
+    book2 = Book("The World According to Garp", 69)
+    book2.turn_page()
+
+    book3 = Book("The Catcher in the Rye", 200)
+    book3.page_count = "not an integer"
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,42 @@
-#!/usr/bin/env python3
-
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self._brand = brand
+        self._size = size
+        self._condition = "Used"
+
+    @property
+    def brand(self):
+        return self._brand
+
+    @property
+    def size(self):
+        return self._size
+
+    @property
+    def condition(self):
+        return self._condition
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            self._size = value
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self._condition = "New"
+
+# Example usage
+if __name__ == "__main__":
+    # Test case 1
+    stan_smith = Shoe("Adidas", 9)
+    assert stan_smith.brand == "Adidas"
+    assert stan_smith.size == 9
+
+    # Test case 2
+    stan_smith.size = "not an integer"  # This will print the error message
+
+    # Test case 3
+    stan_smith.cobble()
+    assert stan_smith.condition == "New"
